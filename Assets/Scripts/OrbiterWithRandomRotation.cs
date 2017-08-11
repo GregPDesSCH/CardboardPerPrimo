9652eab17c5f59cc91208d0adcd9f6829666b0fb
Gregory Desrosiers <gregpdesrosiers@gmail.com>
2017-08-10 19:42:34 -0700

    Added some more planets to the scenes and a new orbit script

---
+++ Assets/Scripts/OrbiterWithRandomRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbiterWithRandomRotation : MonoBehaviour {
+
+    // Orbit rotation speed vector
+    private Vector3 orbitRotationSpeedVector;
+
+    [Range(0.0f, 0.25f)]
+    public float maxOrbitRotationSpeed1;
+
+    [Range(0.0f, 0.25f)]
+    public float maxOrbitRotationSpeed2;
+
+    [Range(0.0f, 0.25f)]
+    public float maxOrbitRotationSpeed3;
+
+    void Start()
+    {
+        orbitRotationSpeedVector = new Vector3(Random.Range(0.0f, maxOrbitRotationSpeed1),
+            Random.Range(0.0f, maxOrbitRotationSpeed2),
+            Random.Range(0.0f, maxOrbitRotationSpeed3));
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(orbitRotationSpeedVector * Time.deltaTime);
+    }
+}
