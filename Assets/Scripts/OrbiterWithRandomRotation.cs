f118e372ebe1b145c5a423c06ed26d7679e4cfe4
Gregory Desrosiers <gregpdesrosiers@gmail.com>
2017-08-12 21:11:55 -0700

    Finished adding more planets to the scenes

--- Assets/Scripts/OrbiterWithRandomRotation.cs
+++ Assets/Scripts/OrbiterWithRandomRotation.cs
@@ -7,13 +7,13 @@ public class OrbiterWithRandomRotation : MonoBehaviour {
     // Orbit rotation speed vector
     private Vector3 orbitRotationSpeedVector;
 
-    [Range(0.0f, 0.25f)]
+    [Range(-0.25f, 0.25f)]
     public float maxOrbitRotationSpeed1;
 
-    [Range(0.0f, 0.25f)]
+    [Range(-0.25f, 0.25f)]
     public float maxOrbitRotationSpeed2;
 
-    [Range(0.0f, 0.25f)]
+    [Range(-0.25f, 0.25f)]
     public float maxOrbitRotationSpeed3;
 
     void Start()
@@ -21,6 +21,15 @@ public class OrbiterWithRandomRotation : MonoBehaviour {
         orbitRotationSpeedVector = new Vector3(Random.Range(0.0f, maxOrbitRotationSpeed1),
             Random.Range(0.0f, maxOrbitRotationSpeed2),
             Random.Range(0.0f, maxOrbitRotationSpeed3));
+
+        if (maxOrbitRotationSpeed1 < 0.0)
+            orbitRotationSpeedVector.x = Random.Range(maxOrbitRotationSpeed1, 0.0f);
+
+        if (maxOrbitRotationSpeed2 < 0.0)
+            orbitRotationSpeedVector.y = Random.Range(maxOrbitRotationSpeed2, 0.0f);
+
+        if (maxOrbitRotationSpeed3 < 0.0)
+            orbitRotationSpeedVector.z = Random.Range(maxOrbitRotationSpeed3, 0.0f);
     }
 
     void FixedUpdate()
